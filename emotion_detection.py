88a8518d4d2a570101a3e1a46d9ddc96c0b2df42
Sanjaygenius777 <unknowngenius777@outlook.com>
2025-06-21 17:57:56 -0400

    complete task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,20 @@
 import requests 
+import json
 
 def emotion_detector(text_to_analyze):
     url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header1={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input1={ "raw_document": { "text": text_to_analyze } }
     response= requests.post(url,headers=header1,json=input1)
-    return response.text
+    res=json.loads(response.text)
+    a=res['emotionPredictions'][0]['emotion']
+    domkey=''
+    maxvalue=0.0
+    for key,value in a.items():
+        if value>maxvalue:
+            maxvalue=value
+            domkey=key
+    
+    b={'dominant_emotion':domkey}
+    emotion=a|b
+    return emotion
