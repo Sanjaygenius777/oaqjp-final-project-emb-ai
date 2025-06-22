b4082d427c9852e90335d66d4f40e59458cabbc0
Sanjaygenius777 <unknowngenius777@outlook.com>
2025-06-22 05:33:58 -0400

    modify task 3 and done task 4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,14 +7,13 @@ def emotion_detector(text_to_analyze):
     input1={ "raw_document": { "text": text_to_analyze } }
     response= requests.post(url,headers=header1,json=input1)
     res=json.loads(response.text)
-    a=res['emotionPredictions'][0]['emotion']
+    emotion=res['emotionPredictions'][0]['emotion']
     domkey=''
     maxvalue=0.0
-    for key,value in a.items():
+    for key,value in emotion.items():
         if value>maxvalue:
             maxvalue=value
             domkey=key
     
-    b={'dominant_emotion':domkey}
-    emotion=a|b
+    emotion['dominant_emotion']=domkey
     return emotion
